bf87a25280ec21d9e0c76b89195f348ec4de9197
Arlind Hajdari <hajdariarlind74@gmail.com>
2026-01-18 15:13:45 +0100

    Add favicon

--- includes/header.php
+++ includes/header.php
@@ -22,6 +22,7 @@ $currentPage = basename($_SERVER['PHP_SELF'], '.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="PharmaCare - Your trusted online pharmacy for medications, vitamins, and health products.">
     <title><?php echo isset($pageTitle) ? sanitize($pageTitle) . ' - ' : ''; ?>PharmaCare Pharmacy</title>
+    <link rel="icon" type="image/svg+xml" href="<?php echo getBaseUrl(); ?>/assets/favicon.svg">
     <link rel="stylesheet" href="<?php echo getBaseUrl(); ?>/assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
